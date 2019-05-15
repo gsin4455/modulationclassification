0392748392c774695cfbf0cd9419a3ec0f0bed0b
gsin4455 <gsin4455@uni.sydney.edu.au>
2019-05-15 17:57:45 +1000

    Add files via upload

--- Mod.py
+++ Mod.py
@@ -1,4 +1,8 @@
+from __future__ import division
+
 import numpy as np
+import scipy
+import matplotlib.pylab as plt
 import random
 
 #this class simulates modulation
@@ -8,7 +12,7 @@ class Mod:
     def __init__(self,x): 
         #put parameters needed for the modulation scheme here
         self.input = x
-        self.omegac= 2*np.pi*2000 
+        self.omegac= 2*np.pi*20000 
         self.output = np.zeros(len(self.input),dtype=float)
 
     def bpsk(self):
@@ -16,15 +20,7 @@ class Mod:
         #modulates signal using binary phase shift keying (i.e sampling 1 bit at a time)
         #transmitted signal
         x = self.input
-        t = np.zeros(len(x),dtype=complex)
-        for i in range(0, len(x)):
-            if (x[i] == 0):
-                t[i] = -1 #phase = -pi
-            else:
-                t[i] = 1 #phase = pi/2
-            
-        #note: I[i]  = 0, Q[i] = (+/-) 1 
-        # -> t[i] = (+/-)1 if mapped on a constellation diagram
+        t = np.where(x==0, -1, 1)
 
         return t
 
@@ -32,38 +28,25 @@ class Mod:
 
         #quadrature amplitude modulation; x = 4
         x = self.input
-        t = np.zeros(len(x),dtype=complex)
+        #t = np.zeros(len(x),dtype=complex)
 
-        a = 0
-        b = 0
-        i = 0
 
-        while i < len(x):
-            #0 -> +1, 1 -> -1  
-            #i.e. 00 -> (1,1), 10 -> (-1,1), 01 -> (1,-1), 11 -> (-1, -1)
-            a = np.power((-1),x[i])
-            b = np.power((-1),x[(i)+1])
-            t[i:(i+1)] = a + b*1j
-            i = i + 2
+        a = np.power(-1, x[0::2])
+        b = np.power(-1, x[1::2])
+        t = a + b*1j
+        
         return t
-
+      
     def qam16(self):
-
+        #00 -> 2, 01 -> 1, 10 -> -1, 11 -> -2  
+        #i.e. 4x4 constellation diagram  
         #quadrature amplitude modulation; x = 16
         x = self.input
-        t = np.zeros(len(x), dtype=complex)
-        a = 0
-        b = 0
-        i = 0
 
-        while i < len(x):
-            #00 -> 2, 01 -> 1, 10 -> -1, 11 -> -2  
-            #i.e. 4x4 constellation diagram  
-            a = np.power((-1),x[i])*(np.abs(np.power((-1),x[i]) + np.power((-1),x[i+1]))) 
-            b = np.power((-1),x[i+2])*(np.abs(np.power((-1),x[i+2]) + np.power((-1),x[i+3])))
-            i = i + 4
-            t[i:(i+3)] = a + b*1j
-
+        a = np.power((-1),x[0::4])*(np.abs(np.power((-1),x[0::4]) + np.power((-1),x[1::4]))) 
+        b = np.power((-1),x[2::4])*(np.abs(np.power((-1),x[2::4]) + np.power((-1),x[3::4])))
+        t = a + b*1j
+    
         return t
 
     def schema(self, scheme):
@@ -81,20 +64,30 @@ class Mod:
         #apply scheme to input to get some modulated output
         y = self.schema(scheme)
         print scheme
-        n = np.arange(0,len(self.input))
+        nsamples = 2048
+        t = np.linspace(0, 5, nsamples)
+        
+        sample_bit = nsamples/len(y)
+        print sample_bit 
+        yr = np.repeat(y, sample_bit)
+        w = np.cos(self.omegac*t)*yr 
 
+        f,ax = plt.subplots(4,1, sharex=True, sharey=True, squeeze=True)
+        ax[0].plot(t, w)
+        ax[0].axis([0, 0.2, -1.5, 1.5])
+        ax[1].plot(t, yr)
+        plt.show()
+        
         #modify this to analog equivalent- multiply by carrier wave
-        self.output = y*np.cos(self.omegac*n) 
-
+        self.output = w
         return self.output         
-
 
 
 
 if __name__ == "__main__":
 #we may compute performance here
-    input = np.random.randint(2, size=256)
-    scheme = 2
+    input = np.random.randint(2, size=64)
+    scheme = 0
 
     modulator = Mod(input)
     output = modulator.apply(scheme)
